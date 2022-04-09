e9c6ff06b3e0c15d59d9af43938ddbc19e6e9509
SavinPark <savinpark0315@gmail.com>
2022-04-09 14:01:09 +0000

    디자인 템플릿 적용

---
+++ src/components/Board.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <section class="bulletin_bord">
+        <div class="container">
+            <h2>게시판</h2>
+            <div class="bord">
+                <ul class="bord_title">
+                    <li>No</li>
+                    <li>제목</li>
+                    <li>작성자</li>
+                    <li>날짜</li>
+                </ul>
+                <ul v-for="(post, idx) in board" :key="idx">
+					<li><p>{{ post.no }}</p></li>
+                    <li><p>{{ post.title }}</p></li>
+                    <li><p>{{ post.writer }}</p></li>
+                    <li><p>{{ post.date }}</p></li>
+				</ul>
+            </div>
+            <div class="number_tab">
+                <button class="before_btn">&lt;</button>
+                <ul>
+                    <button class="active_color">1</button>
+                    <button>2</button>
+                    <button>3</button>
+                </ul>
+                <button class="after_btn">&gt;</button>
+            </div>
+        </div>
+    </section> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'board',
+  data: function() {
+	  return {
+		  board : [
+			  {no: 1, title: '안녕!!', contents: '신짱구가 작성한 글', writer: '신짱구', date: '2022.03.19'},
+			  {no: 2, title: '반갑습니다! 김철수 입니다.', contents: '김철수가 작성한 글', writer: '김철수', date: '2022.03.16'},
+			  {no: 3, title: '안녕하세요~! 유리에요~', contents: '한유리가 작성한 글', writer: '한유리', date: '2022.03.16'},
+			  {no: 4, title: '반갑습니다..이훈이훈이훈', contents: '이훈이 작성한 글', writer: '이훈', date: '2022.03.16'},
+			  {no: 5, title: '안녕하세요. 반가워요 "한수지"에요.', contents: '한수지가 작성한 글', writer: '한수지', date: '2022.03.16'},
+		  ],
+	  }
+  },
+}
+</script>
---
+++ src/components/Form.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <section class="bord_form">
+        <div class="container">
+            <h2>등록하기</h2>
+            <div class="formbox">
+                <form>
+                    <p><span>제 목</span><input class="form_element" type="text" name="title"></p>
+                    <p><span>작 성 자</span><input class="form_element" type="text" name="user_name"></p>
+                    <p class="text_area"><span>내 용</span><textarea class="form_element" name="user_content"></textarea></p>
+                </form>
+            </div>
+            <div class="form_btn">
+                <button>삭제</button>
+                <button>등록</button>
+                <button>수정</button>
+                <button>취소</button>
+            </div>
+        </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'form',
+}
+</script>
